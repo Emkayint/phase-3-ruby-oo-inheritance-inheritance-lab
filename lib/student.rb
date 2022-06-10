c89ae71da095eacc4cf89e1ae971c8e5441376c1
Martin <mkmartinoes@gmail.com>
2022-06-10 19:40:05 +0300

    Complited Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user"
+
+class Student < User
+
+  attr_reader :knowledge
+  attr_reader :learn
+  # attr_accessor :str
+
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(str)
+    # self.str = str
+    @knowledge << str
+  end
 
 end
